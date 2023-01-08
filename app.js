79d2fe9cc391350616bb68f68c3a0378d7a716af
Nathan Hedge <23344786+10Nates@users.noreply.github.com>
2023-01-08 00:19:32 -0600

    woops no port

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use("/api", apiRouter);
 app.use("/top", topRouter);
 app.use("/NULL", NullRouter);
 
-// const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 if (!process.env.GITHUB_TOKEN) {
   // Unauthorized github requests have a rate limit 
